3cccff2819d5ddae3fdba557473aaf486deb0edb
Aramis Romero Carballea <aromero@pyxelsolution.com>
2022-04-21 15:56:01 -0400

    add snackbar service

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
-    import { Styles } from 'sveltestrap';
-  </script>
-  
-  <Styles />
-  <slot></slot>
+import SnackBar from "$lib/components/SnackBar.svelte";
+
+ 
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+</svelte:head>
+<SnackBar></SnackBar>
+<slot />
+
+<style>
+
+  :root {
+    --toastContainerTop: auto;
+    --toastContainerRight: auto;
+    --toastContainerBottom: 1rem;
+    --toastContainerLeft: calc(50vw - 8rem);
+  }
+</style>
